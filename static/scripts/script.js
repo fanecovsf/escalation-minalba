ba5e696797aed5f401f70e9a61e327b0d96e148c
nohranferreira <nohran1708@hotmail.com>
2023-08-15 21:35:11 -0300

    feat: conexão com API e apresentando contatos na tela

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,6 +1,6 @@
 //endpoint contatos
  let contatos = []
-
+const elementoParaInserirContato = document.getElementById("contato")
 const endPointAPIContato = 'https://escalation-minalba.onrender.com/minalba/contatos'
 
 getBuscarContatos()
@@ -9,7 +9,26 @@ async function getBuscarContatos() {
     const res = await fetch(endPointAPIContato)
     contato = await res.json()
     console.table(contato)
+    exibirContatosnaTela(contato)
 
+}
+
+function exibirContatosnaTela(Contatos) {
+    Contatos.forEach(contato => {
+        elementoParaInserirContato.innerHTML += `
+        <div class="blog_post" id="contato">
+            <div class="container_copy">
+                <h3>${contato.nivel}</h3>
+                <h1>${contato.nome}</h1>
+                <p>${contato.cargo}</p>
+                <p>${contato.email}</p>
+                <p>${contato.telefone}</p>
+                <p>${contato.turno}</p>
+                <a class="btn_primary" href='#' target="_blank"><i class="fa-solid fa-pen"></i></a>
+            </div>
+        </div>
+        `
+    });
 }
 
 //endpoint unidades
