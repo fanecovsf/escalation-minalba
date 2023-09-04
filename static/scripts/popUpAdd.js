230a77f62b58e34f8faf789bd26d4cef090f42f2
nohranferreira <nohran1708@hotmail.com>
2023-09-04 17:40:59 -0300

    🌌: realizado a inclusão do PUT no fronte end

--- static/scripts/popUpAdd.js
+++ static/scripts/popUpAdd.js
@@ -1,11 +1,35 @@
 const button = document.querySelector("button");
 const modal = document.querySelector("dialog");
 const buttonClose = document.getElementById("close");
+const salvarBtn = document.getElementById("salvar");
 
 button.onclick = function () {
-	modal.showModal();
+  modal.showModal();
 };
 
 buttonClose.onclick = function () {
-	modal.close();
+  modal.close();
 };
+
+salvarBtn.addEventListener("click", function (evento) {
+  evento.preventDefaut();
+
+  const formNome = document.getElementById("name").value;
+  const formNivel = document.getElementById("nivel").value;
+  const formCargo = document.getElementById("cargo").value;
+  const formEmail = document.getElementById("email").value;
+  const formTelefone = document.getElementById("phone").value;
+  const formTurno = document.getElementById("turno").value;
+  const formUnidade = document.getElementById("unidade").value;
+
+  const requestOptions = {
+    method: "PUT",
+    body: JSON.stringify({
+      nome,
+      nivel,
+      cargo,
+      email,
+      phone,
+    }),
+  };
+});
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,32 +2,55 @@
 let contatos = [];
 const elementoParaInserirContato = document.getElementById("contato");
 const endPointAPIContato =
-	"https://escalation-minalba.onrender.com/minalba/contatos";
+  "https://escalation-minalba.onrender.com/minalba/contatos";
 
 getBuscarContatos();
 
 async function getBuscarContatos() {
-	const res = await fetch(endPointAPIContato);
-	contato = await res.json();
-	console.table(contato);
-	exibirContatosnaTela(contato);
+  const res = await fetch(endPointAPIContato);
+  contato = await res.json();
+  console.table(contato);
+  exibirContatosnaTela(contato);
+}
+
+const endPointAPIAdd =
+  "https://escalation-minalba.onrender.com/minalba/contatos/add";
+
+getBuscarContatos();
+
+async function getBuscarContatos() {
+  const res = await fetch(endPointAPIAdd);
+  addContato = await res.json();
+  console.table(addContato);
+  exibirContatosnaTela(addContato);
 }
 
 let unidades = [];
 
 const endPointAPIUnidade =
-	"https://escalation-minalba.onrender.com/minalba/unidades";
+  "https://escalation-minalba.onrender.com/minalba/unidades";
 
 getBuscarUnidades();
 async function getBuscarUnidades() {
-	const respostaUnidade = await fetch(endPointAPIUnidade);
-	unidade = await respostaUnidade.json();
-	console.table(unidade);
+  const respostaUnidade = await fetch(endPointAPIUnidade);
+  unidade = await respostaUnidade.json();
+  console.table(unidade);
+
+  const selecaoUnidade = document.getElementById("unidade");
+
+  selecaoUnidade.innerHTML = "";
+
+  unidade.forEach((elemento) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = elemento.id;
+    optionElement.textContent = elemento.nome;
+    selecaoUnidade.appendChild(optionElement);
+  });
 }
 
 function exibirContatosnaTela(Contatos) {
-	Contatos.forEach((contato) => {
-		elementoParaInserirContato.innerHTML += `
+  Contatos.forEach((contato) => {
+    elementoParaInserirContato.innerHTML += `
         <div class="blog_post" id="contato">
             <div class="container_copy">
                 <h3>Nível: ${contato.nivel}</h3>
@@ -40,7 +63,54 @@ function exibirContatosnaTela(Contatos) {
             </div>
         </div>
         `;
-	});
+  });
 }
-// <a class="btn_primary" href='#' target="_blank"><i class="fa-solid fa-pen"></i></a>
-//endpoint unidades
+
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonClose = document.getElementById("close");
+const salvarBtn = document.getElementById("salvar");
+
+button.onclick = function () {
+  modal.showModal();
+};
+
+buttonClose.onclick = function () {
+  modal.close();
+};
+
+salvarBtn.addEventListener("click", function (evento) {
+  evento.preventDefault();
+
+  const nome = document.getElementById("name").value;
+  const nivel = document.getElementById("nivel").value;
+  const cargo = document.getElementById("cargo").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("phone").value;
+  const turno = document.getElementById("turno").value;
+  const unidade = document.getElementById("unidade").value;
+  const unidadeNumero = Number(unidade);
+
+  //   console.log(unidade.value);
+
+  const requestOptions = {
+    method: "PUT",
+    body: {
+      nome: nome,
+      cargo: cargo,
+      nivel: nivel,
+      turno: turno,
+      email: email,
+      telefone: telefone,
+      unidade_id: unidadeNumero,
+    },
+  };
+  fetch(endPointAPIAdd, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(requestOptions["body"]);
+      alert("Dados enviados com sucesso!");
+      modal.close(); // Fechar o diálogo
+    })
+    .catch((error) => alert("Falha ao enviar dados: ", error));
+});
